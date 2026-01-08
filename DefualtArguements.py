0a781bb31f860f79be439e1f2773f2f6c0b70338
Suryansh Singh <mrsurya0509@gmail.com>
2026-01-08 08:15:16 +0530

    Update DefualtArguements.py

--- DefualtArguements.py
+++ DefualtArguements.py
@@ -1,7 +1,8 @@
-# Arguements And Parameters
+# Default Parameters
 def sum ( a, b, d=56 ): # Defualt ParaMeters
     c= a+b+d
     print(f" the output is sum of a={ a } and b={b} and d={d}= {c}")
 
 sum ( 34,343) # Defualt Arguements
+
 sum ( b= 34, a= 3443, d=343) # Keyword Arguements
